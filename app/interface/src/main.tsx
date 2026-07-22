6e6da1f3b2f748d0cb2688ce32ed51ad349858f9
Abel Cañas <abelstor@gmail.com>
2026-07-22 15:52:33 +0200

    feat: configure TanStack Query
    - Add QueryClientProvider with 5min stale time
    - Wrap app in main.tsx
    - Prepared for mock data hooks in future tasks

--- app/interface/src/main.tsx
+++ app/interface/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryProvider } from '@/providers/QueryProvider'
 import './index.css'
 import App from './App'
 
@@ -11,6 +12,8 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>,
 )
